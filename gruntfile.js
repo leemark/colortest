206025256a2dfeb1b8dd8c2ce0d22b4828574a38
leemark <leemark@gmail.com>
2014-01-09 22:31:18 -0700

    switched css to sass

--- gruntfile.js
+++ gruntfile.js
@@ -15,9 +15,30 @@ module.exports = function(grunt) {
                 src: 'js/build/production.js',
                 dest: 'js/build/production.min.js'
             }
+        },
+        watch: {
+            scripts: {
+                files: ['js/*.js', 'css/*.scss'],
+                tasks: ['concat', 'uglify', 'sass'],
+                options: {
+                    spawn: false,   
+                }
+            }
+        },
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed' 
+                },
+                files: {
+                    'css/build/styles.css': 'css/styles.scss'
+                }
+            }
         }
     });
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.registerTask('default', ['concat', 'uglify']);
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.registerTask('default', ['concat', 'uglify', 'sass']);
 };
